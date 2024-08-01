61fe78ceda2a30053d1e56fbd2a760eb46bef1b4
Karel <karelkarunia24@gmail.com>
2024-08-02 00:56:55 +0200

    fix misspelled props

--- app/components/LoaderScreen.tsx
+++ app/components/LoaderScreen.tsx
@@ -2,10 +2,10 @@ const LoaderScreen = () => {
     return (
         <div
             id={"loader-screen"}
-            className="w-screen h-screen bg-black grid place-content-center fixed inset-0 z-50 font-serif"
+            className="w-dvh h-dvh bg-black grid place-content-center fixed inset-0 z-50 font-serif"
         >
             <svg viewBox="0 0 400 400">
-                <text name="text-body" x="40%" y="40%" text-anchor="middle">
+                <text name="text-body" x="40%" y="40%" textAnchor="middle">
                     Karel
                 </text>
                 <text
@@ -13,7 +13,7 @@ const LoaderScreen = () => {
                     x="50%"
                     y="40%"
                     dy="0.9em"
-                    text-anchor="middle"
+                    textAnchor="middle"
                 >
                     &
                 </text>
@@ -22,7 +22,7 @@ const LoaderScreen = () => {
                     x="60%"
                     y="40%"
                     dy="1.9em"
-                    text-anchor="middle"
+                    textAnchor="middle"
                 >
                     Sabrina
                 </text>
--- app/svg/MusicIcon.tsx
+++ app/svg/MusicIcon.tsx
@@ -4,9 +4,9 @@ const MusicIcon = () => {
             <g
                 id="Page-1"
                 stroke="none"
-                stroke-width="1"
+                strokeWidth="1"
                 fill="none"
-                fill-rule="evenodd"
+                fillRule="evenodd"
             >
                 <g
                     id="Dribbble-Light-Preview"
