9b045c1a6281a87ccce5b69c08580bcba7473f48
Karel Karunia <karelkarunia24@gmail.com>
2025-01-23 22:35:54 +0100

    Adds key to mapped bubbletext

--- app/components/wish/Wishes.tsx
+++ app/components/wish/Wishes.tsx
@@ -5,7 +5,11 @@ const Wishes = ({ wishes }: { wishes: Array<Wish> }) => {
     return (
         <div className="max-h-[50%] space-y-2 overflow-y-auto p-2">
             {wishes.map((wish) => (
-                <BubbleText name={wish.name} message={wish.wish} />
+                <BubbleText
+                    key={wish.name}
+                    name={wish.name}
+                    message={wish.wish}
+                />
             ))}
         </div>
     );
