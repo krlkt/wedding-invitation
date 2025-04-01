efb687447876f1bc2b068bee43fe8705e699bb55
Karel Karunia <karelkarunia24@gmail.com>
2025-04-01 17:09:34 +0200

    fix wish order bug

--- app/components/wish/action.ts
+++ app/components/wish/action.ts
@@ -5,7 +5,7 @@ import { Wish } from '@/app/models/wish';
 import { revalidatePath } from 'next/cache';
 
 export const addWish = async (data: Wish) => {
-    await query('INSERT INTO wish (name, wish) VALUES ($name, $wish)', {
+    await query('INSERT INTO wish (name, wish, created_at) VALUES ($name, $wish, CURRENT_TIMESTAMP)', {
         name: data.name,
         wish: data.wish,
     });
