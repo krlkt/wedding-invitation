c389be43d7855d92a3b6ef5e1b7a177e44e60813
Karel <karelkarunia24@gmail.com>
2025-05-24 11:37:23 +0200

    install datagrid and add dummy data

--- app/dashboard/DashboardPage.tsx
+++ app/dashboard/DashboardPage.tsx
@@ -1,10 +1,32 @@
 'use client';
 import { useState } from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { RSVP } from '../models/rsvp';
+
+const rows: GridRowsProp<RSVP> = [
+    { id: '1', name: 'Karel', attend: 'yes', guest_number: 2, notes: '' },
+    { id: '2', name: 'Aidos', attend: 'yes', guest_number: 2, notes: 'I am a vegan' },
+    { id: '3', name: 'Crazy guy', attend: 'yes', guest_number: 4, notes: 'I am a vegan but I eat fish' },
+];
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 50 },
+    { field: 'name', headerName: 'Name', width: 300 },
+    { field: 'attend', headerName: 'Attend?', width: 100 },
+    { field: 'guest_number', headerName: 'Number of guest', width: 100 },
+    { field: 'notes', headerName: 'Notes', width: 400 },
+];
 
 const DashboardPage = () => {
-    const [authenticated, setAuthenticated] = useState<boolean>(false);
+    const [authenticated, setAuthenticated] = useState<boolean>(true);
 
-    return <div>Dashboard</div>;
+    return authenticated ? (
+        <div style={{ height: 300, width: '100%' }}>
+            <DataGrid rows={rows} columns={columns} />
+        </div>
+    ) : (
+        <div>Login Page</div>
+    );
 };
 
 export default DashboardPage;
