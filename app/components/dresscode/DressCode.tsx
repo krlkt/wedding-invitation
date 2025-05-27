f8f47045805492dcea3331f3792d38b644b288e0
Karel Karunia <karelkarunia24@gmail.com>
2025-05-27 23:28:04 +0200

    update men dresscode

--- app/components/dresscode/DressCode.tsx
+++ app/components/dresscode/DressCode.tsx
@@ -35,14 +35,14 @@ const MenWomenSection = ({ sex }: { sex: 'men' | 'women' }) => (
             <h6 className="text-2xl text-blue-800">{sex === 'women' ? 'Women' : 'Men'}</h6>
             <p className="text-sm">
                 {sex === 'women'
-                    ? 'Something pastel, Avoid loud prints & patterns, wedges or heels'
-                    : 'Plain button shirt (blazer or coat optional) pair with Closed toe shoes. As long as its formal honestly.. 😄'}
+                    ? 'Something pastel, avoid loud prints & patterns 💕. Bridesmaids, please use the baby blue dresses 👗 '
+                    : 'Plain button shirt any color (blazer or vest optional) pair with closed toe shoes. 😄 Groomsmen, please use black tuxedo suit, bow tie will be provided 🤵 '}
             </p>
         </div>
         <div className="w-1/2 h-full aspect-[1/1] relative p-2 border border-primary-main rounded-lg">
             <div className="w-full h-full relative overflow-hidden">
                 <Image
-                    src={sex === 'women' ? '/images/dresscode/women.jpg' : '/images/dresscode/women.png'}
+                    src={sex === 'women' ? '/images/dresscode/women.jpg' : '/images/dresscode/men.jpg'}
                     alt={'Women example dresscode'}
                     fill
                     className="rounded-lg object-bottom"
