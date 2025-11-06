ed23d8f54c12a82b931e9c7d6c6be67e30f36e41
Karel <karelkarunia24@gmail.com>
2025-11-06 20:28:59 +0100

    quickfix: remove default creds: remove default credentials

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -95,18 +95,13 @@ function LoginFormContent() {
           </button>
 
           {/* Registration Link */}
-          <div className="pt-4 text-center text-sm text-gray-600">
+          <div className="text-center text-sm text-gray-600">
             Don&apos;t have an account?{' '}
             <Link href="/register" className="font-semibold text-pink-500 hover:text-pink-600">
               Create your wedding website
             </Link>
           </div>
         </form>
-
-        <div className="mt-6 text-center text-sm text-gray-600">
-          <p>Default credentials:</p>
-          <p className="mt-1 font-mono text-xs">karel@wedding.com / changeme123</p>
-        </div>
       </div>
     </div>
   )
