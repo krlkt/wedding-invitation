c7547adf684d625b5af45e9cd64dad114870d531
Karel Karunia <karelkarunia24@gmail.com>
2025-11-16 00:56:21 +0100

    OIAL-16: ref: deleted unneeded migration

--- app/db/migrations/0001_stormy_bromley.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `wedding_configurations` ADD `monogram_filename` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `monogram_file_size` integer;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `monogram_mime_type` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `groom_father` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `groom_mother` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `bride_father` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `bride_mother` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `footer_text` text;--> statement-breakpoint
-ALTER TABLE `wedding_configurations` ADD `instagram_link` text;
