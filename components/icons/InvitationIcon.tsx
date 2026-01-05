ccf7ee4780f278631ef570f2a82902560bbbb8f9
Karel Karunia <karelkarunia24@gmail.com>
2026-01-05 01:27:20 +0100

    OIAL-17: fix invitation icon

--- components/icons/InvitationIcon.tsx
+++ components/icons/InvitationIcon.tsx
@@ -1,15 +1,10 @@
-const InvitationIcon = ({ color = 'currentColor' }: { color?: string }) => (
+const InvitationIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke={color}
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    fill="currentColor"
   >
-    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
-    <circle cx="12" cy="10" r="3" />
+    <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
   </svg>
 );
 
