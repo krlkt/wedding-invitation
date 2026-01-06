5068694ed2689fbe3139c6eeb1ee009f1776c02b
Karel Karunia <karelkarunia24@gmail.com>
2026-01-06 22:08:55 +0100

    quickfix: trying fix on migration sql

--- db/migrations/0004_purple_robbie_robertson.sql
+++ db/migrations/0004_purple_robbie_robertson.sql
@@ -1,11 +1,11 @@
-DROP INDEX "bride_section_content_wedding_config_id_unique";--> statement-breakpoint
-DROP INDEX "bank_details_wedding_config_id_unique";--> statement-breakpoint
-DROP INDEX "dress_codes_wedding_config_id_unique";--> statement-breakpoint
-DROP INDEX "groom_section_content_wedding_config_id_unique";--> statement-breakpoint
-DROP INDEX "user_accounts_email_unique";--> statement-breakpoint
-DROP INDEX "wedding_configurations_user_id_unique";--> statement-breakpoint
-DROP INDEX "wedding_configurations_subdomain_unique";--> statement-breakpoint
-DROP INDEX "starting_section_content_wedding_config_id_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "bride_section_content_wedding_config_id_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "bank_details_wedding_config_id_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "dress_codes_wedding_config_id_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "groom_section_content_wedding_config_id_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "user_accounts_email_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "wedding_configurations_user_id_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "wedding_configurations_subdomain_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "starting_section_content_wedding_config_id_unique";--> statement-breakpoint
 ALTER TABLE `love_story_segments` ALTER COLUMN "date" TO "date" text NOT NULL;--> statement-breakpoint
 CREATE UNIQUE INDEX `bride_section_content_wedding_config_id_unique` ON `bride_section_content` (`wedding_config_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `bank_details_wedding_config_id_unique` ON `bank_details` (`wedding_config_id`);--> statement-breakpoint
